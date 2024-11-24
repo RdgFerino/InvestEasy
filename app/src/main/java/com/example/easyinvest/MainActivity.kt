19739fba67eca07e34098d4158e9218d1ce872fc
Raoni gomes oliveira ferino <raoniferino@gmail.com>
2024-11-24 16:32:38 +0000

    App investeasy em desenvolvimento

---
+++ app/src/main/java/com/example/easyinvest/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.easyinvest
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.ViewCompat.*
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.LENGTH_LONG
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
+
